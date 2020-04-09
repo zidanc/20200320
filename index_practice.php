dbb064d1ed42ce9384f292b334317b67ad5cdfd0
zidanc <b93605079@g.ntu.edu.tw>
2020-04-09 17:32:39 +0800

    變數與常數練習

--- index_practice.php
+++ index_practice.php
@@ -1,6 +1,25 @@
 <?php
-
+//宣告一個變數
 $a=1;
 echo $a;
+echo "<br>";
+$a=2.1;
+echo $a;
+echo "<br>";
+echo "<hr>";
+$a=1;
+echo $a;
+echo "<br>";
+$a=3.14;
+echo $a;
+echo "<br>";
+/*
+宣告一個常數，
+常數不可被變更。
+*/
+define("PI",3.1415927);
+echo PI;
+echo "<br>";
+echo PI*2*10;
 
 ?>

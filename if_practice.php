c5974f764fe4cab9cf25c943727cdda4fec2abcd
zidanc <b93605079@g.ntu.edu.tw>
2020-04-11 23:47:04 +0800

    if判斷式、switch、三元運算子

--- if_practice.php
+++ if_practice.php
@@ -56,7 +56,70 @@ if($year%4 == 0){
 公元年份除以100可整除但除以400不可整除，為平年。
     ->公元年份除以4可整除，並且 (除以100不可整除 或是 除以400可以整除)，為閏年。
     */
+echo "<hr>";
 
+/*
+100~90 -> “A”
+75~90 -> “B”
+60~75 ->”C”
+0~59 –>”D”
+
+A,非常好，請努力保持
+B,可圈可點，但還有進步的空間
+C,一般水平，需要更多的努力
+D,用心不足，請加強
+*/
+
+$score= 70;
+$level= "";
+if ($score>=90) {
+  $level="A";
+}elseif ($score>=75) {
+  $level="B";
+}elseif ($score>=60) {
+  $level="C";
+}else{
+  $level="D";
+}
+echo "成績：" . $score;
+echo "<br>";
+echo "等級：" . $level; 
+echo "<br>";
+
+switch($level){
+  case "A":
+    echo "非常好，請努力保持";
+  break;
+  case "B":
+    echo "可圈可點，但還有進步的空間";
+  break;
+  case "C":
+    echo "一般水平，需要更多的努力";
+  break;
+  case "D":
+    echo "用心不足，請加強";
+  break;
+}
+
+echo "<hr>";
+// 三元運算子
+
+$score= 80;
+echo ($score>=60)? "及格":"不及格";
+
+echo "<hr>";
+
+$score= 85;
+$result= "";
+
+if($score>=60){
+  
+  $result= "及格";
+
+}else{
+  $result= "不及格";
+}
+  echo $result;
 
 
 ?>

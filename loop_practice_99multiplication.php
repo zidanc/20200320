1c0b181e4e1b6b8632d40b87411b4506e27ee838
zidanc <b93605079@g.ntu.edu.tw>
2020-04-12 16:54:51 +0800

    九九乘法表變形+合併CSS

--- loop_practice_99multiplication.php
+++ loop_practice_99multiplication.php
@@ -1,3 +1,18 @@
+<style>
+  table{
+    border: 1px solid black;
+    text-align: center;
+  }
+  table td{
+    border: 1px solid #888;
+    /* border-collapse: collapse; 沒有效果？*/
+  }
+  tr:nth-child(1),td:nth-child(1){
+    background: #EEE;
+  }
+  
+</style>
+
 <?php
 /* 外迴圈包內迴圈，共兩層的巢狀迴圈
 for($i=0;$i<10;$i++){
@@ -10,10 +25,51 @@ for($i=0;$i<10;$i++){
 // multiplication table
 for($i=1;$i<=9;$i++){
   for($j=1;$j<=9;$j++){
-    echo $i . " x " . $j . " = " . ($i*$j) . " , " ;
+    echo $j . " x " . $i . " = " . ($i*$j) . " , ";
   }
   echo "<br>";
 }
 
+echo "<table>";
+for($i=1;$i<=9;$i++){
+  echo "<tr>";
+  for($j=1;$j<=9;$j++){
+    echo "<td>" . $i . " x " . $j . " = " . ($i*$j) . " , " . "</td>";
+  }
+  // echo "<br>";
+  echo "</tr>";
+}
+echo "</table>";
+
+echo "<table border='1'>";
+for($i=1;$i<=9;$i++){
+  echo "<tr>";
+  for($j=1;$j<=9;$j++){
+    echo "<td>" . $i . " x " . $j . " = " . ($i*$j) . "</td>";
+  }
+  // echo "<br>";
+  echo "</tr>";
+}
+echo "</table>";
+echo "<hr>";
+
+echo "<table border='1'>";
+for ($i=0; $i <=9 ; $i++) {
+  if ($i==0) {
+    echo "<tr><td>" . " " ."</td>";
+  }else{
+    echo "<tr><td>". $i ."</td>"; 
+
+  }
+  for ($j=1; $j <=9 ; $j++) { 
+    if ($i == 0){
+      echo "<td>" . 1*$j . "</td>";
+    }else{
+      echo "<td>" . $i*$j . "</td>";
+    }
+  }
+  echo "</tr>";
+}
+echo "</table>";
 
 ?>

986b854ed1bdf380ed4bc6f998879b8729138d41
zidanc <b93605079@g.ntu.edu.tw>
2020-04-20 07:12:49 +0800

    中空矩形方法二

--- star_practice.php
+++ star_practice.php
@@ -49,17 +49,54 @@ for ($i=1; $i <=5 ; $i++) {
   echo "<br>";
 }
 
-//菱形
-for ($i=1; $i <=5 ; $i++) { 
-  for ($k=1; $k <=(5-$i) ; $k++) { 
+echo "<hr>";
+echo "菱形"."<br>";
+
+for ($i=1; $i <=9 ; $i++) { 
+      if ($i<=5) {
+        $t=$i;
+      }else{
+        $t=10-$i;
+      }
+  for ($k=1; $k <=5-$t ; $k++) { 
     echo "&nbsp;";
   }
-  for ($j=1; $j <=(2*$i-1) ; $j++) { 
+  for ($j=1;$j<=2*$t-1;$j++){
+
     echo "*";
   }
   echo "<br>";
 }
 
- 
+echo "<hr>";
+echo "矩形";
+echo "<br>";
+
+for ($i=1; $i <=7 ; $i++) { 
+  for ($j=1; $j <=7 ; $j++) { 
     
+    if ($i==1 || $i==7) {
+      echo "*";
+    }else if($j==1 || $j==7){
+      echo "*";
+    }else{
+      echo "&nbsp;";
+    }
+
+  }
+  echo "<br>";
+}
+
+echo "<hr>";
+echo "中空矩形作法二";
+echo "<br>";
+for($i=1;$i<=7;$i++){
+  if ($i==1 || $i==7) {
+    echo "*";
+  }else if($k){
+    
+  }
+}
+
+
 ?>

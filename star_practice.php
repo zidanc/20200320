7a6c8ceaf40d447b97cb689d8e868e551cda14b6
Zidan <b93605079@g.ntu.edu.tw>
2020-04-20 16:31:04 +0800

    新增練習題

--- star_practice.php
+++ star_practice.php
@@ -88,7 +88,7 @@ for ($i=1; $i <=7 ; $i++) {
 }
 
 echo "<hr>";
-echo "中空矩形作法二";
+echo "中空矩形作法二: 巢狀迴圈";
 echo "<br>";
 for($i=1;$i<=7;$i++){
 
@@ -136,8 +136,90 @@ for($i=1;$i<=7;$i++){
 }
 
 echo "<hr>";
+echo "矩形帶對角線";
+echo "<br>";
 
+for ($i=1; $i <=7 ; $i++) { 
+  for ($j=1; $j <=7 ; $j++) { 
+    
+    if ($i==1 || $i==7 || $i==$j) {
+      echo "*";
+    }else if($j==1 || $j==7 || $j==8-$i){
+      echo "*";
+    }else{
+      echo "&nbsp;";
+    }
+
+  }
+  echo "<br>";
+}
+ 
+echo "錯誤語法，導致矩形帶對角線變成無限實心矩形";
+echo "<br>";
+
+// for ($i=1; $i <=7 ; $i++) { 
+//   for ($j=1; $j <=7 ; $j++) { 
+    
+//     if ($i==1 || $i==7 || $i=$j) {
+//       echo "*";
+//     }else if($j==1 || $j==7 || $j==8-$i){
+//       echo "*";
+//     }else{
+//       echo "&nbsp;";
+//     }
+
+//   }
+//   echo "<br>";
+// }
+
+echo "<hr>";
+echo "矩形帶對角線作法二: 巢狀迴圈";
+echo "<br>";
+for($i=1;$i<=7;$i++){
+
+  if ($i==1 || $i==7) {
   
+    for ($j=1; $j <=7 ; $j++) { 
+      echo "*";
+    }
+    
+  }else{
+  
+    for ($k=1;$k<=7;$k++) {
+  
+      if ($k==1 || $k==7 || $k==$i || $k==(8-$i)) { 
+        echo "*";
+      }else{
+        echo "&nbsp;";
+      }
+    }   
+  }
+  echo "<br>";
+}
+echo "<hr>";
+echo "修改code，讓未來改變一個變數即可正常改變矩形大小且帶對角線";
+echo "<br>";
+
+$x=14;
+
+for ($i=1; $i <=$x ; $i++) { 
+  for ($j=1; $j <=$x ; $j++) { 
+    
+    if ($i==1 || $i==$x || $i==$j) {
+      echo "*";
+    }else if($j==1 || $j==$x || $j==($x+1)-$i){
+      echo "*";
+    }else{
+      echo "&nbsp;";
+    }
+
+  }
+  echo "<br>";
+}
+echo "綜合菱形及矩形的做法，撰寫一個挖空的菱形程式";
+echo "<hr>";
+echo "承上題，在挖空的菱形中加入對角線";
+echo "<br>";
 
 
 ?>

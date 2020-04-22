e6fc886b05ade76a44577058392814f1a6ae2505
Zidan <b93605079@g.ntu.edu.tw>
2020-04-22 15:01:10 +0800

    陣列複習1st_pra1

---
+++ pra1_practice.php
@@ -0,0 +1,15 @@
+<?php
+$a=[];
+for ($i=1;$i<=9;$i++){
+  for($j=1;$j<=9;$j++){
+    $a[]= $i . "x" . $j . "=" . ($i*$j);
+  }
+}
+
+echo "<pre>";print_r($a);"</pre>";
+echo "<hr>";
+
+echo $a[31];
+$a[31]=9999;
+echo "<pre>";print_r($a);"</pre>";
+?>

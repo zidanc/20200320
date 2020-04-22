0c3e74bdba789dc84ff116466b1d0971a5a4eac2
Zidan <b93605079@g.ntu.edu.tw>
2020-04-22 16:30:40 +0800

    陣列複習1st_pra1

--- pra1_practice.php
+++ pra1_practice.php
@@ -6,10 +6,21 @@ for ($i=1;$i<=9;$i++){
   }
 }
 
-echo "<pre>";print_r($a);"</pre>";
-echo "<hr>";
+// echo "<pre>";print_r($a);"</pre>";
+// echo "<hr>";
 
-echo $a[31];
-$a[31]=9999;
-echo "<pre>";print_r($a);"</pre>";
+// echo $a[31];
+// $a[31]=9999;
+// echo "<pre>";print_r($a);"</pre>";
+
+for ($i=0;$i<count($a);$i++){
+  echo $a[$i] . " , ";
+  if ($i%9 == 8) {
+    echo "<br>";
+  }
+}
+
+foreach($a)
+
+
 ?>

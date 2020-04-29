bf6b94db015c2fd16b33b5dbeb3f20e3b78490c2
Zidan <b93605079@g.ntu.edu.tw>
2020-04-29 09:40:29 +0800

    新增資料至資料庫的資料表

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>月曆的月份轉換</title>
-</head>
-<body>
- 
- <?php
+<?php
 
-  $month=date("m");  /*取電腦本機系統月份*/
-  
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,"root","");
+date_default_timezone_set("Asia/Taipei");
 
+$sql="insert into students(`id`,
+                          `acc`,
+                          `pw`,
+                          `name`,
+                          `email`,
+                          `tel`,
+                          `create_time`,
+                          `update_time`,
+                          `birthday`) 
+                   values(null,
+                          'zidan',
+                          '456456',
+                          '簡志瀚',
+                          'b93605079@g.ntu.edu.tw',
+                          '0960533552',
+                          '".date("Y-m-d H:i:s")."',
+                          '".date("Y-m-d H:i:s")."',
+                          '1990-01-01')";
 
+echo $sql;
+echo "<br>";
+// $pdo->query($sql);
+echo $pdo->exec($sql);  // 不回傳資料，但回傳成功或失敗 (0 or 1)//
+echo "資料已新增";
 
- ?>
-
-</body>
-</html>
+?>

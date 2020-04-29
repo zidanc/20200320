40ba79aa7d7c3fdb53462fcf62b20495160ce3c7
Zidan <b93605079@g.ntu.edu.tw>
2020-04-29 14:47:41 +0800

    會員列表、編輯會員

--- edit_user.php
+++ edit_user.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>編輯會員</title>
   <style>
     fieldset{
       width: 300px;
@@ -22,35 +22,45 @@
   </style>
 </head>
 <body>
-  <h1></h1>
+  <!-- <h1></h1> -->
+  <?php
+    $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo=new PDO($dsn,"root","");
+    date_default_timezone_set("Asia/Taipei");
+
+    $id=$_GET['user'];
+    $sql="select * from `students` where `id`='$id'";
+    $user=$pdo->query($sql)->fetch();
+
+  ?>
   <fieldset>
-    <legend>註冊會員</legend>
+    <legend>編輯會員</legend>
       <form method="POST" enctype="multipart/form-data" action="add_user.php">
       <table>
         <tr>
           <td>帳號：</td>
-          <td><input type="text" name="acc" value="" placeholder="請輸入帳號" required></td>
+          <td><input type="text" name="acc" value="<?=$user['acc'];?>" placeholder="請輸入帳號" required></td>
         </tr>
         <tr>
           <td>密碼：</td>
-          <td><input type="password" name="pw" value="" placeholder="請輸入密碼" required></td>
+          <td><input type="password" name="pw" value="<?=$user['pw'];?>" placeholder="請輸入密碼" required></td>
         </tr>
         <tr>
           <td>姓名：</td>
-          <td><input type="text" name="name" value="">
+          <td><input type="text" name="name" value="<?=$user['name'];?>">
           </td>
         </tr>
         <tr>
           <td>出生日：</td>
-          <td class="birthday"><input type="date" name="birth"></td>
+          <td class="birthday"><input type="date" name="birth" value="<?=$user['birthday'];?>"></td>
         </tr>
         <tr>
           <td>聯絡信箱：</td>
-          <td><input type="email" name="email" required></td>
+          <td><input type="email" name="email" value="<?=$user['email'];?>" required></td>
         </tr>
         <tr>
           <td>手機：</td>
-          <td><input type="text" name="tel" value=""></td>
+          <td><input type="text" name="tel" value="<?=$user['tel'];?>"></td>
         </tr>
       </table>
       
@@ -72,3 +82,5 @@ date_default_timezone_set("Asia/Taipei");
 
 
 ?>
+</body>
+</html>

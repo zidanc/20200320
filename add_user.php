cdc898c098768c1b40ac09353e4f23afb6581aa4
Zidan <b93605079@g.ntu.edu.tw>
2020-04-29 11:25:55 +0800

    使用表單來新增使用者

---
+++ add_user.php
@@ -0,0 +1,35 @@
+<?php
+
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,"root","");
+date_default_timezone_set("Asia/Taipei");
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$tel=$_POST['tel'];
+$birthday=$_POST['birthday'];
+
+
+echo $acc;
+echo $pw;
+echo $name;
+echo $email;
+echo $tel;
+echo $birthday;
+
+$sql="insert into `students` (`acc`,`pw`,`name`,`email`,`tel,`create_time`,`update_time`,`birthday`) values('$acc','$pw','$name','$email','$tel','".date("Y-m-d H:i:s")."','".date("Y-m-d H:i:s")."','$birthday')";
+
+$res=$pdo->exec($sql);
+echo "<br>";
+
+if($res>=1){
+  echo $res;
+  echo "刪除成功!";
+}else{
+  echo $res;
+  echo "刪除失敗!";
+}
+
+?>
--- reg.php
+++ reg.php
@@ -1,3 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    table{
+      /* border-collapse: collapse; */
+    }
+    td{
+      width: 100px;
+      /* border: 1px solid gray; */
+      text-align: right;
+    }
+    td.birthday{
+      text-align: left;
+    }
+  </style>
+</head>
+<body>
+  <h1></h1>
+  <fieldset>
+    <legend>註冊會員</legend>
+      <form method="POST" enctype="multipart/form-data">
+      <table>
+        <tr>
+          <td>帳號：</td>
+          <td><input type="text" name="acc" value="" placeholder="請輸入帳號" required></td>
+        </tr>
+        <tr>
+          <td>密碼：</td>
+          <td><input type="password" name="pw" value="" placeholder="請輸入密碼" required></td>
+        </tr>
+        <tr>
+          <td>姓名：</td>
+          <td><input type="text" name="name" value="">
+          </td>
+        </tr>
+        <tr>
+          <td>出生日：</td>
+          <td class="birthday"><input type="date" name="birth"></td>
+        </tr>
+        <tr>
+          <td>聯絡信箱：</td>
+          <td><input type="email" name="email" required></td>
+        </tr>
+        <tr>
+          <td>手機：</td>
+          <td><input type="text" name="tel" value=""></td>
+        </tr>
+      </table>
+      
+      
+      <div>
+        <input type="submit" value="送出">
+        <input type="reset" value="重置">
+      </div>
+
+</body>
+</html>
+
+
+
 <?php
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,"root","");

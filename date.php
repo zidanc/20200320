fe333eafef9bc8ff89afcd57835879bc6e7ba6d8
zidanc <b93605079@g.ntu.edu.tw>
2020-05-04 05:42:24 +0800

    date函式、strtotime函式的偏移

--- date.php
+++ date.php
@@ -17,18 +17,36 @@ echo date("Y年n月j日",$serialTime);
 <div>距離下一次的生日還有幾天</div>
 <?php
 
-
-$today="2020-05-03";
-$nextBirth="2020-07-02";
+$today=date("Y-m-d");
+// $today="2020-05-03";        //左邊這樣寫代表以當天是00小時：00分：00秒來做計算。
+$nextBirth="2020-07-02";    //兩天都是00小時：00分：00秒來相減，這樣後續換算除以86400會是整數天。
 
 $between=strtotime($nextBirth)-strtotime($today);
 echo $between."<br>";
 
 $days=$between/86400;      //一天=24hr*60min*60sec=86400 sec.
 echo "還剩".$days."天";
+echo "<hr>";
+//運用date("z",時間序)來運算，z:年份中的第幾天。
+$days=date("z",strtotime($nextBirth))-date("z",strtotime($today));
+echo $days;
+?>
 
+<hr>
+<h4>date()函式</h4>
 
+<?php
+date_default_timezone_set("Asia/Taipei");
+echo date("Y-m-d H:i:s");
+?>
 
+<hr>
+<h4>strtotime()函式的偏移</h4>
+
+<?php
+echo strtotime("now")."<br>";
+$date=strtotime("+3 days");
+echo date("Y-m-d",$date);        
 
 
 ?>

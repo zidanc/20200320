71d17e7353ca9595ce768ce614e015234ef8a48f
zidanc <zidanc@hotmail.com>
2020-05-08 00:53:25 +0800

    多檔案之間傳值的好處

--- student.php
+++ student.php
@@ -5,6 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>學生成績查詢</title>
 </head>
+
+<!-- 你會發現此檔案幾乎沒有php程式，只有html，也就是說其實附檔名可以改成html是Ok的。 -->
+<!-- 另外，由於一個人負責的程式行數有限，並且前台使用介面、後台資料庫功能最好能夠拆分讓不同coder分工，這樣專案才可以同時合作進行，縮短開發時間。 -->
+<!-- 還有依照功能拆分檔案的好處是，未來管理檔案及debug也比較能快速尋找出、辨別錯誤點。 -->
+
 <body>
   <form action="score.php" method="get">
     學生座號：<input type="number" name="num" id="">

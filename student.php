519a82a88375d5d33843dd0ce9584e4bcd9c67c2
zidanc <zidanc@hotmail.com>
2020-05-08 00:43:56 +0800

    學生成績查詢--多檔案頁面傳值練習

---
+++ student.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>學生成績查詢</title>
+</head>
+<body>
+  <form action="score.php" method="get">
+    學生座號：<input type="number" name="num" id="">
+    <input type="submit" value="查詢">
+  </form>
+</body>
+</html>

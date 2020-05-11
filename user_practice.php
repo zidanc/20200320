af693792ebf28a7971cd6d37613fe47704c5948d
zidanc <zidanc@hotmail.com>
2020-05-12 06:56:24 +0800

    早期登入頁面的檢查機制

--- user_practice.php
+++ user_practice.php
@@ -1,5 +1,19 @@
 <?php
 
-
+if (!empty($_POST)) {
+  $acc=$_POST["acc"];
+  $pw=$_POST["pw"];
+  
+  if ($acc=="admin" && $pw=="1234") {
+    
+    echo "登入成功";
+    
+  }else{
+    
+    echo "acc=".$acc."<br>";
+    echo "pw=".$pw."<br>";
+    echo "登入失敗，請輸入正確帳號、密碼";
+  }
+}
 
 ?>

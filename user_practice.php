894eb70db16d6e143aea7bbf266d7975c2a14222
zidanc <zidanc@hotmail.com>
2020-05-13 07:04:45 +0800

    表單傳值、網址傳值概念

--- user_practice.php
+++ user_practice.php
@@ -1,5 +1,7 @@
 <?php
 
+$status="fail";   //預設$status="fail";狀態。
+
 if (!empty($_POST)) {
   $acc=$_POST["acc"];
   $pw=$_POST["pw"];
@@ -7,13 +9,16 @@ if (!empty($_POST)) {
   if ($acc=="admin" && $pw=="1234") {
     
     echo "登入成功";
-    
+    $status="login";
+
   }else{
     
     echo "acc=".$acc."<br>";
     echo "pw=".$pw."<br>";
     echo "登入失敗，請輸入正確帳號、密碼";
+
   }
 }
 
 ?>
+<a href="login_practice.php?status=<?=$status;?>">回首頁</a>

a7b2d44e3a8b708e354e4a0ca2c2120e3cb9db81
zidanc <zidanc@hotmail.com>
2020-05-21 04:38:20 +0800

    撈出二位陣列的方式之一

--- practice/query.php
+++ practice/query.php
@@ -1,10 +1,15 @@
 <?php
 
-$dsn="mssql:host:localhost;charset=utf8;dbname=school";
+$dsn="mysql:host:localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,"root","");
 date_default_timezone_set("Asia/Taipei");
 
 $sql="select `name`,`tel` from `student`";
 
+$rows=$pdo->query($sql)->fetchAll();
+
+foreach ($rows as $r ) {
+  echo $r['name']."-".$r['tel']."<br>";
+}
 
 ?>

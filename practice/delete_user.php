11984c540f8ce30d5d596721395962a56c703fda
zidanc <zidanc@hotmail.com>
2020-05-22 00:46:40 +0800

    刪除功能完成

--- practice/delete_user.php
+++ practice/delete_user.php
@@ -4,7 +4,18 @@ $pdo=new PDO($dsn,"root","");
 date_default_timezone_set("Asia/Taipei");
 
 
-$del_user=$_GET['user'];
+$del_id=$_GET['user'];
+$sql="delete from `student` where `id`='$del_id'";
 
+$res=$pdo->exec($sql);
+
+if ($res>=1) {
+  header("location:list_user.php");
+} else {
+  echo "刪除失敗！<br>";
+  echo "可能原因：1-該筆資料已不存在。2-資料庫連線有問題。3-SQL語法有誤。";
+  echo $sql;
+}
+//我們寫這些程式，是讓使用者不用自己寫程式、也不用進到資料庫，也能做CRUD。
 
 ?>

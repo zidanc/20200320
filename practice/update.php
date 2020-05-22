c4c57460d4afde239f7584107e7091557021fbad
zidanc <zidanc@hotmail.com>
2020-05-23 01:56:46 +0800

    修正新增會員資料與釐清query & exec

--- practice/update.php
+++ practice/update.php
@@ -4,14 +4,18 @@ $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,"root","");
 date_default_timezone_set("Asia/Taipei");
 
-$sql="update `student` set `name`='olivia'";
-// echo $sql;
+$sql="update `student` set `name`='olivia' ";
+// echo $sql; 這樣的update語法，是全部name欄位都改成olivia。
 
 $rows=$pdo->exec($sql);
+echo $rows;
 
-foreach ($rows as $r) {
-  echo $r['name']."-".$r['tel']."<br>";
-}
+// foreach ($rows as $r) {   
+//   echo $r['name']."-".$r['tel']."<br>";
+// }
+// exec()是回傳影響筆數，整數值，並非陣列。因此第十三行寫foreach函式就會出現無效參數代入foreach()的警告！
+
+
 
 // 更新一筆成功就會回傳1，更新兩筆成功就會回傳2 
 // if ($rows >= 1) {

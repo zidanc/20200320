13f26f1036defd9b78e729e9d765aac0158e821c
zidanc <zidanc@hotmail.com>
2020-06-01 06:53:17 +0800

    月曆修繕程式碼+假日待完工

---
+++ calendar/holiday.php
@@ -0,0 +1,22 @@
+<?php
+
+$holiday=[];
+// $holiday[$mon][$num]
+$holiday[1][1]="元旦";
+$holiday[2][28]="和平紀念日";
+$holiday[3][8]="婦女節";
+$holiday[3][12]="國父逝世|植樹節";
+$holiday[4][4]="兒童節";
+$holiday[4][5]="清明節";
+$holiday[5][1]="勞動節";
+$holiday[8][8]="父親節";
+$holiday[9][28]="教師節";
+$holiday[10][10]="國慶日";
+$holiday[10][25]="光復節";
+$holiday[10][25]="萬聖節";
+$holiday[11][12]="國父誕辰紀念日";
+$holiday[12][24]="平安夜";
+$holiday[12][25]="行憲紀念日";
+
+
+?>
